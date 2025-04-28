a30959e4464a1aeca6ae01354a32f469cc4b30dd
Ayush Dwivedi <dwivediayush028@gmail.com>
2025-04-28 14:56:35 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,13 @@
-
-import React from "react";
+import React, { useState } from "react";  
 import './../styles/App.css';
+import Login from "./Login"; 
 
 const App = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);  
+
   return (
-    <div>
-        {isLoggedIn ? (
+    <div id="main">
+      {isLoggedIn ? (
         <h2>Welcome! You are logged in.</h2>
       ) : (
         <Login isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn} />
@@ -14,4 +16,4 @@ const App = () => {
   )
 }
 
-export default App
+export default App;
